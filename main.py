e9481e29ac40f927fb4dfc052581b989c60f43b5
Andes Court <andres@andrescourt.com>
2022-06-20 12:18:17 -0500

    CRUD best practice and validation

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
+from typing import Optional
 from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # Order matters, first written is what serves
+
+# title: string
+# content: string
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+
+my_posts = [
+    {
+        "id": 1,
+        "title": "title of post 1",
+        "content": "content of post 2"
+    },
+    {
+        "id": 2,
+        "title": "favorite foods",
+        "content": "I like pizza"
+    }
+]
 
 
 @app.get("/")
@@ -13,17 +39,16 @@ def root():
 @app.get("/posts")
 def get_posts():
     return {
-        "data": "This is your post"
+        "data": my_posts
     }
 
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    # print(payload)
+@app.post("/posts")
+# def create_posts(payload: dict = Body(...)):
+def create_posts(post: Post):
+    print(post)
+    print(post.content)
+    print(post.dict())
     return {
-        "message": "succesfully created post",
-        "new_post": {
-            "title": payload['title'],
-            "content": payload['content']
-        }
+        "data": post
     }
